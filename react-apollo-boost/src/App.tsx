2cb99e8e8a8913289ace417b23212183f2ecf89a
ShotaIuchi <shotaiuchi.develop@gmail.com>
2022-05-24 20:31:34 +0900

    sample code
    https://zenn.dev/ut0n/articles/d99f1c5bc0ef51

--- react-apollo-boost/src/App.tsx
+++ react-apollo-boost/src/App.tsx
@@ -1,26 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import gql from "graphql-tag";
+
+console.log(process.env.REACT_APP_GITHUB_TOKEN);
+
+type Repository = {
+  name: string;
+  url: string;
+  description: string;
+};
+
+const GET_REPOSITORY = gql`
+  query {
+    viewer {
+      repositories(
+        first: 3
+        privacy: PUBLIC
+        isFork: false
+        orderBy: { field: CREATED_AT, direction: DESC }
+      ) {
+        nodes {
+          name
+          url
+          description
+        }
+      }
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: "https://api.github.com/graphql",
+  request: (operation) => {
+    operation.setContext({
+      headers: {
+        authorization: `Bearer ${process.env.REACT_APP_GITHUB_TOKEN}`,
+      },
+    });
+  },
+});
 
 function App() {
+  const [repositories, setRepositories] = React.useState<Repository[]>([]);
+
+  const fetch = async () => {
+    const { data } = await client.query({
+      query: GET_REPOSITORY,
+    });
+    console.log(data);
+    setRepositories(data.viewer.repositories.nodes);
+  };
+
+  React.useEffect(() => {
+    fetch();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        {repositories.map((repository: Repository) => {
+          return (
+            <div style={style} key={repository.name}>
+              <b>Repository: {repository.name}</b>
+              <p>URL: {repository.url}</p>
+              <p>Description: {repository.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
+
+const style = {
+  margin: "8px auto",
+  padding: "8px",
+  border: "1px solid #000",
+  width: "500px",
+};
 
 export default App;
